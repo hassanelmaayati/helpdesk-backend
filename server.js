1e7ab63a80b2482080cfba57fc93207223a30ee4
LockVision <hassanelmaayati@gmail.com>
2026-09-12 22:45:33 +0300

    Connect Express server to MongoDB using Mongoose

---
+++ server.js
@@ -0,0 +1,26 @@
+
+require('dotenv').config()
+const express =require ('express')
+const mongoose=require('mongoose')
+const app=express()
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected',()=>{
+  console.log('Connected to MongoDB :D')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3001,()=>{
+  console.log('server is running on port 3001 ;)')
+})
