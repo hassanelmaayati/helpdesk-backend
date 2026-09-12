d29da0d41f789e75e6a8ebeb9ed5890b72701019
LockVision <hassanelmaayati@gmail.com>
2026-09-12 23:08:15 +0300

    Add signup route with bcrypt password hashing

---
+++ controllers/authController.js
@@ -0,0 +1,27 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+
+const signup = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    res.status(201).json({
+      _id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      role: newUser.role,
+    });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+module.exports = { signup };
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
+
+app.use(express.json());
+app.use('/auth', authRoutes);
+
+
+
+
+
+
 
 mongoose.connect(process.env.MONGODB_URI);
 
