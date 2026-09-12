dcea3c4c18d62daa1e041b6b97787eb6a860d9e8
LockVision <hassanelmaayati@gmail.com>
2026-09-12 23:32:40 +0300

    Add login route with password comparison and JWT generation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const jwt=require('jsonwebtoken')
 
 const signup = async (req, res) => {
   try {
@@ -24,4 +25,54 @@ const signup = async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 };
-module.exports = { signup };
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({
+      
+      _id: user._id, role: user.role
+     },
+
+      process.env.JWT_SECRET,
+      { expiresIn: '24h' }
+
+    );
+
+
+
+    res.status(200).json({
+       token, user: { _id: user._id, name: user.name, email: user.email, role: user.role 
+
+       }});
+
+  } catch (err) {
+
+    res.status(400).json({ error: err.message });
+
+  }
+
+
+
+
+
+
+
+  
+};
+module.exports = { signup, login };
