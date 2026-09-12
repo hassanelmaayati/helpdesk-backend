8870efdd7a472f2afd7c807333f47162ad0e2a2c
LockVision <hassanelmaayati@gmail.com>
2026-09-12 23:47:59 +0300

    Add JWT verification middleware and protected test route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { signup, login } = require('../controllers/authController');
+const verifyToken = require('../middleware/verifyToken');
 
 router.post('/signup', signup);
 router.post('/login', login);
+
+router.get('/me', verifyToken, (req, res) => {
+  res.status(200).json({ message: 'You are authenticated', user: req.user });
+});
 
 module.exports = router;
