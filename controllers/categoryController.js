fda059b93a7f228bdfc16dc3ba7f85c6db67950b
zainab-16-marker <zaynp01602@gmail.com>
2026-09-13 11:24:59 +0300

    Add category CRUD controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -24,8 +24,40 @@ res.status(500).json({ error: err.message });
 }
 }
 
+const update = async (req,res)=>{
+    try{
+        if (req.user.role !== 'it-staff') {
+         return res.status(403).json({ error: 'Access denied' });}
+         const updatedCategory = await Category.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        //  if(!updatedCategory){
+        // return res.status(404).json({ error: 'Category not found' });
+        //  }
+         res.status(200).json(updatedCategory);
+
+    }
+    catch(err){
+    res.status(500).json({ error: err.message });
+    }
+}
+
+
+const deleteCategory = async (req,res)=>{
+    try{
+    if (req.user.role !== 'it-staff') {
+    return res.status(403).json({ error: 'Access denied' });}
+    const deletCategory=await Category.findByIdAndDelete(req.params.id)
+     res.status(200).json(deletCategory)
+
+    }
+    catch(err){
+    res.status(500).json({ error: err.message });
+
+    }
+}
 
 module.exports={
     index,
     create,
+    update,
+    deleteCategory,
 }
