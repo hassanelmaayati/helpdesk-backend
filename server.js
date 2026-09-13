936407fcf98ef424f7300e92c17f1b26523ef108
zainab-16-marker <zaynp01602@gmail.com>
2026-09-13 14:22:13 +0300

    finish with the category routes

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require('./routes/authRoutes');
-
+const categoryRoutes = require('./routes/categoryRoutes');
 const app = express();
 
 app.use(express.json());
 app.use('/auth', authRoutes);
-
-
+app.use('/categories', categoryRoutes);
 
 
 
