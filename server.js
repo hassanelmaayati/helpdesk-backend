d1e5f2c1e2370560cc2ba9f4c1ed8f3b98d09a68
LockVision <hassanelmaayati@gmail.com>
2026-09-15 13:45:53 +0300

    deployment

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const authRoutes = require('./routes/authRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
-const commentRouter = require('./routes/commentRouter');
-const ticketRoutes = require('./routes/ticketRoutes');
+const authRoutes = require("./routes/authRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+const commentRouter = require("./routes/commentRouter");
+const ticketRoutes = require("./routes/ticketRoutes");
 
 const app = express();
+const isProduction = process.env.NODE_ENV === "production";
 
+if (isProduction) {
+  app.set("trust proxy", 1);
+}
 app.use(express.json());
-app.use('/auth', authRoutes);
-app.use('/categories', categoryRoutes);
-app.use('/tickets', ticketRoutes);
+app.use("/auth", authRoutes);
+app.use("/categories", categoryRoutes);
+app.use("/tickets", ticketRoutes);
 
-
-app.use('/', commentRouter);
-
+app.use("/", commentRouter);
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -23,6 +25,6 @@ mongoose.connection.on("connected", () => {
   console.log("Connected to MongoDB :D");
 });
 
-app.listen(3001, () => {
-  console.log("server is running on port 3001 ;)");
+app.listen(3001, "0.0.0.0", () => {
+  console.log(`The express app is ready on port ${port}!`);
 });
