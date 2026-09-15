95adc441c864fdf8a9442f4c7b2ffaf4ab810203
zainab-16-marker <zaynp01602@gmail.com>
2026-09-15 13:02:03 +0300

    Resolve server merge conflict

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const authRoutes = require('./routes/authRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
 const commentRouter = require('./routes/commentRouter');
@@ -8,14 +10,13 @@ const ticketRoutes = require('./routes/ticketRoutes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.use('/auth', authRoutes);
 app.use('/categories', categoryRoutes);
 app.use('/tickets', ticketRoutes);
-
-
 app.use('/', commentRouter);
-
 
 mongoose.connect(process.env.MONGODB_URI);
 
